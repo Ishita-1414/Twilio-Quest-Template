cbdc92fb2acfd4e0bd7c999eb6d95e8c5ed1d747
Legedith <jatindehmiwal@gmail.com>
2021-06-27 04:03:01 +0530

    Made a map and stuff

--- levels/vr_mission_template/objectives/example_objective2/validator.js
+++ levels/vr_mission_template/objectives/example_objective2/validator.js
@@ -18,21 +18,15 @@ have completed the challenge as instructed.
 */
 module.exports = async function (helper) {
   // We start by getting the user input from the helper
-  const { answer1, answer2 } = helper.validationFields;
+  const { answer1} = helper.validationFields;
 
   // Next, you test the user input - fail fast if they get one of the
   // answers wrong, or some aspect is wrong! Don't provide too much
   // negative feedback at once, have the player iterate.
-  if (!answer1 || !isTwilio(answer1)) {
-    return helper.fail(`
-      The answer to the first question is incorrect. The company that
-      makes TwilioQuest starts with a "T" and ends with a "wilio".
-    `);
-  }
 
   // You can use npm or core Node.js dependencies in your validators!
   try {
-    assert.strictEqual(R.add(2, 2), Number(answer2));
+    assert.strictEqual(NUmber(233168), Number(answer1));
   } catch (e) {
     return helper.fail(`
       The second answer you provided was either not a number, or not the
